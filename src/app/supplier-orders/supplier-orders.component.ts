7713f4d1927dd19ded8f88ac068d49e67fb5553f
Pallavi <PallaviS2@hexaware.com>
2021-02-12 19:08:07 +0530

    another update

--- src/app/supplier-orders/supplier-orders.component.ts
+++ src/app/supplier-orders/supplier-orders.component.ts
@@ -1,4 +1,10 @@
+import { OrdersService } from './../orders.service';
+import { SupplierService } from './../supplier.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Orders } from './../order-details/orders';
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { Supplier } from '../supplier-dash/Supplier';
 
 @Component({
   selector: 'app-supplier-orders',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupplierOrdersComponent implements OnInit {
 
-  constructor() { }
+  oId: number;
+  sId: number;
+  s: Supplier;
+  orders: Observable<Orders[]>;
+
+  constructor(private router: Router, private route: ActivatedRoute,
+              private supplierService: SupplierService, private orderService: OrdersService) { }
 
   ngOnInit(): void {
+    this.reloadData();
   }
 
+  goHome() {
+    this.router.navigate(['/suppdash', this.sId]);
+  }
+
+  loadDetails() {
+    this.router.navigate(['/suppdetails', this.sId]);
+  }
+
+  reloadData() {
+
+    // tslint:disable-next-line: radix
+    this.sId = parseInt(this.route.snapshot.paramMap.get('id'));
+
+    this.supplierService.getSupplierById(this.sId)
+      .subscribe(data => {
+        console.log(data);
+        this.s = data;
+      }, error => console.log(error));
+
+    this.orderService.getSupplierOrders(this.sId)
+      .subscribe(data => {
+        console.log(data);
+        this.orders = data;
+      }, error => console.log(error));
+  }
+
+  showOrderDetails(id: number, sup: number) {}
+
+  showPendingOrders() {}
 }
